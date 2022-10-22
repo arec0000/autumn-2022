1c429cae7165d37bc5253fb15b6e655e00b09dc5
Nikita <s2015836@x.donstu.ru>
2022-10-22 19:19:21 +0300

    Add domain in nginx conf and lentsencrypt init

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(pinpinim.ru www.pinpinim.ru) # !!! поменять домен
+domains=(grtsk.ru www.grtsk.ru)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="arec0000@gmail.com" # Adding a valid address is strongly recommended
