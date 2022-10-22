b43770be164fbd1c6e0641ff7753b3434480247d
Nikit099 <nechit1337@gmail.com>
2022-10-22 12:18:15 +0300

    admin page

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,23 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Auth from './components/pages/auth/auth'
-import Register from './components/pages/register/register'
+import Auth from "./components/pages/auth/auth";
+import Register from "./components/pages/register/register";
+import Admin from "./components/pages/admin/admin";
 
-import './assets/style.scss'
+import "./assets/style.scss";
 
 const App = () => {
     return (
         <div className="App">
             <Router>
                 <Routes>
-                    <Route path="/auth" element={<Auth/>}/>
-                    <Route path="/register" element={<Register/>}/>
+                    <Route path="/auth" element={<Auth />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/admin" element={<Admin />} />
                 </Routes>
             </Router>
         </div>
-    )
-}
+    );
+};
 
-export default App
+export default App;
