42c0c7c30f18e50919554729a17f2aac89032a5e
Nikita <s2015836@x.donstu.ru>
2022-10-22 22:08:22 +0300

    Add achievements

--- client/src/components/pages/achievements/achievements.js
+++ client/src/components/pages/achievements/achievements.js
@@ -1,9 +1,45 @@
 import './achievements.scss'
 
-const Achievements = () => {
+const Achievements = ({achievements}) => {
     return (
         <div className="achievements">
             <h2>Достижения</h2>
+            <div className="achievements__block">
+                <span className="achievements__date">
+                    15 сентября
+                </span>
+                <div className="achievements__line"/>
+                <span className="achievements__description">
+                    Какое-то достижение
+                </span>
+            </div>
+            <div className="achievements__block">
+                <span className="achievements__date">
+                    15 сентября
+                </span>
+                <div className="achievements__line"/>
+                <span className="achievements__description">
+                    Какое-то достижение
+                </span>
+            </div>
+            <div className="achievements__block">
+                <span className="achievements__date">
+                    15 сентября
+                </span>
+                <div className="achievements__line"/>
+                <span className="achievements__description">
+                    Какое-то достижение
+                </span>
+            </div>
+            <div className="achievements__block">
+                <span className="achievements__date">
+                    15 сентября
+                </span>
+                <div className="achievements__line"/>
+                <span className="achievements__description">
+                    Какое-то достижение
+                </span>
+            </div>
         </div>
     )
 }
