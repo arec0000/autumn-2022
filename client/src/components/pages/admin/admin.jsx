243d20b3ec541b2bfd5f9ad37958451538a6f410
Nikit099 <nechit1337@gmail.com>
2022-10-23 13:12:50 +0300

    made create user

--- client/src/components/pages/admin/admin.jsx
+++ client/src/components/pages/admin/admin.jsx
@@ -7,35 +7,37 @@ const Admin = () => {
     const [nw, setNew] = useState({title: '', text: ''});
     const [tokenFlag, setTokenFlag] = useState(false)
     const [tegs, setTegs] = useState(['социальная жизнь', 'учебные новости', 'жизнь ВУЗа' ])
-    const [tegFlag, setTegFlag] = useState(false)
+    const [valueUser, setValueUser] = useState({name: '', lastName: '', group: '', email: ''})
     const [valueToken, setValueToken] = useState([{
         check: false,
         id: 1,
-        label: "Админ"
+        label: "Сотрудник"
     },
     {
         check: false,
         id: 2,
-        label: "Учитель"
+        label: "Преподаватель"
     },
     {
         check: false,
         id: 3,
-        label: "Ученик"
+        label: "Студент"
     },
 ])
-    const [users] = useState([
+    const [users, setUsers] = useState([
         {
             name: "Вова",
             lastName: "Смирнов",
-            grup: "21HT",
+            group: "21HT",
             email: "123456Nn@gmail.com",
+            id: 1
         },
         {
             name: "Андрей",
             lastName: "Карагачев",
-            grup: "21VQ",
+            group: "21VQ",
             email: "123456Ak@gmail.com",
+            id: 2
         },
     ]);
     const [news, setNews] = useState([
@@ -82,17 +84,34 @@ const Admin = () => {
             setNew({title: edNew.title, text: edNew.text})
         }
         
-
+    }
+    function removeUser(id){
+        setUsers(users.filter(u => u.id !== id))
+    }
+    function createUser(){
+        if (valueUser.email && valueUser.group && valueUser.lastName && valueUser.name){
+            const newUser = {
+                name: valueUser.name,
+                lastName: valueUser.lastName,
+                group: valueUser.group,
+                email: valueUser.email,
+                id: new Date().getTime()
+            }
+            setUsers(prev => [...prev, newUser])
+            setValueUser({name: '',
+                lastName: '',
+                group: '',
+                email: '',})
+        }
     }
     return (
         <div className="adminContainer">
-            
             <div className="adminRow">
                 
                 <BlockComments comments={comments}/>
-                <BlockNews setTegFlag={setTegFlag} setNews={setNews} tegFlag={tegFlag} tegs={tegs} news={news} editNew={editNew} nw={nw} setNew={setNew} title={nw.title} text={nw.text} createNew={createNew} />
+                <BlockNews setNews={setNews}  tegs={tegs} news={news} editNew={editNew} nw={nw} setNew={setNew} title={nw.title} text={nw.text} createNew={createNew} />
                 
-                <UsersList valueToken={valueToken} setValueToken={setValueToken} tokenFlag={tokenFlag} setTokenFlag={setTokenFlag} users={users} />
+                <UsersList createUser={createUser} setValueUser={setValueUser} valueUser={valueUser} removeUser={removeUser} valueToken={valueToken} setValueToken={setValueToken} tokenFlag={tokenFlag} setTokenFlag={setTokenFlag} users={users} />
             </div>
 
         </div>
